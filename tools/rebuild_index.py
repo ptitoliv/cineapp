22ea1609db983af84c770af5e04dc0d4c5af080f
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2021-03-22 22:36:06 +0100

    Add script for rebuilding FTS index
    Fixes: #116

---
+++ tools/rebuild_index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import datetime
+from flask_msearch import Search
+from cineapp import app
+from cineapp import models
+
+search = Search()
+search.init_app(app)
+
+search.create_index(models.Movie)
