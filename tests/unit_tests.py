29fd2275b2785df0901a1abc60669a9e243f6a74
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2025-07-26 14:46:52 +0200

    Reorganise tests
    Put activity flow test after homework flow test in order to have a
    better code coverage.
    
    Fixes #150

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -519,39 +519,7 @@ class FlaskrTestCase(unittest.TestCase):
         rv=self.app.get('/logout', follow_redirects=True)
         assert "Welcome to CineApp" in str(rv.data)
 
-    def test_19_activity_flow(self):
-
-        """
-            Display activity flow and data 
-        """
-
-        # Login
-        rv=self.app.post('/login',data=dict(username="ptitoliv",password="toto1234"), follow_redirects=True)
-        assert "Welcome <strong>ptitoliv</strong>" in str(rv.data) 
-
-        # Check if the activity flow route is working
-        modes={ 'tvshow': 'séries', 'movie': 'films' };
-
-        # Switch between availables modes
-        for key, value in modes.items():
-
-            # Let's change mode
-            rv=self.app.get('/switch/%s' % key, follow_redirects=True)
-            rv=self.app.get('/activity/show', follow_redirects=True)
-            assert "Flux d&#39;activité des %s" % value in str(rv.data.decode('utf-8'))
-        
-        # Now test the datatable part
-        args={'draw': 1, 'columns': [{'data': 'entry_type', 'name': '', 'searchable': True, 'orderable': False, 'search': {'value': '', 'regex': False, 'fixed': []}}, {'data': None, 'name': '', 'searchable': True, 'orderable': False, 'search': {'value': '', 'regex': False, 'fixed': []}}, {'data': 'entry_text', 'name': '', 'searchable': True, 'orderable': False, 'search': {'value': '', 'regex': False, 'fixed': []}}], 'order': [], 'start': 0, 'length': 100, 'search': {'value': '', 'regex': False, 'fixed': []}}
-
-        rv=self.app.post('/activity/update', data=dict(args=json.dumps(args)),headers=[('X-Requested-With', 'XMLHttpRequest')], follow_redirects=True)
-        response_args=json.loads(rv.data)["data"]
-        assert len(response_args) > 0
-
-        # Logout
-        rv=self.app.get('/logout', follow_redirects=True)
-        assert "Welcome to CineApp" in str(rv.data)
-
-    def test_20_homework(self):
+    def test_19_homework(self):
 
         """
             Display activity flow and data 
@@ -620,6 +588,38 @@ class FlaskrTestCase(unittest.TestCase):
         with mail.record_messages() as outbox:
             rv=self.app.get('/homework/delete/3/2',follow_redirects=True)
             assert "Impossible de supprimer le devoir - Une note existe déjà" in rv.data.decode('utf-8')
+
+        # Logout
+        rv=self.app.get('/logout', follow_redirects=True)
+        assert "Welcome to CineApp" in str(rv.data)
+
+    def test_20_activity_flow(self):
+
+        """
+            Display activity flow and data 
+        """
+
+        # Login
+        rv=self.app.post('/login',data=dict(username="ptitoliv",password="toto1234"), follow_redirects=True)
+        assert "Welcome <strong>ptitoliv</strong>" in str(rv.data) 
+
+        # Check if the activity flow route is working
+        modes={ 'tvshow': 'séries', 'movie': 'films' };
+
+        # Switch between availables modes
+        for key, value in modes.items():
+
+            # Let's change mode
+            rv=self.app.get('/switch/%s' % key, follow_redirects=True)
+            rv=self.app.get('/activity/show', follow_redirects=True)
+            assert "Flux d&#39;activité des %s" % value in str(rv.data.decode('utf-8'))
+        
+        # Now test the datatable part
+        args={'draw': 1, 'columns': [{'data': 'entry_type', 'name': '', 'searchable': True, 'orderable': False, 'search': {'value': '', 'regex': False, 'fixed': []}}, {'data': None, 'name': '', 'searchable': True, 'orderable': False, 'search': {'value': '', 'regex': False, 'fixed': []}}, {'data': 'entry_text', 'name': '', 'searchable': True, 'orderable': False, 'search': {'value': '', 'regex': False, 'fixed': []}}], 'order': [], 'start': 0, 'length': 100, 'search': {'value': '', 'regex': False, 'fixed': []}}
+
+        rv=self.app.post('/activity/update', data=dict(args=json.dumps(args)),headers=[('X-Requested-With', 'XMLHttpRequest')], follow_redirects=True)
+        response_args=json.loads(rv.data)["data"]
+        assert len(response_args) > 0
 
         # Logout
         rv=self.app.get('/logout', follow_redirects=True)
