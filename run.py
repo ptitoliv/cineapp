297786b4ecbcc4f5bfb84e70d5b6c84ff5eb382d
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2025-10-05 16:26:00 +0200

    run.py - Launch application with parameters
    Due to the refactoring, it is now possible to launch the application
    with a config file as parameter using --config parameter.
    
    Fixes #152

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
-from cineapp import app, socketio
+import argparse
+from cineapp import socketio, create_app
+
+# Parse the command line
+parser = argparse.ArgumentParser()
+parser.add_argument('--config')
+args=parser.parse_args()
+
+# Create the application
+app = create_app(args.config)
 
 # We need to run the application with SocketIO object
 # If not the apply doesn't work well on socket event detection
