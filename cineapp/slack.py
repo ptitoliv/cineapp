4be63a1e2f488f45bc0816e618ab00a21e3db4de
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2026-06-13 23:26:59 +0200

    feat(videogame) Generate specific Slack message for videogame
    igdb.com blocks SlackBot. So it's not possible to display a link preview
    like it is possible with tvdb.com. So let's generate a specific card
    when a mark for a videogame is published on slack with the poster
    fetched for the cineapp /static/posters directory.
    
    Fixes #153

--- cineapp/slack.py
+++ cineapp/slack.py
@@ -1,5 +1,6 @@
 from builtins import str
 from builtins import object
+from flask import request
 from slack import WebClient
 import json
 
@@ -11,10 +12,10 @@ class SlackChannel(object):
 
                 self.channel_name = channel_name
                 self.slack_token = None
-                
+
                 # This is here we are going to send the Slack notification
                 self.slack_token = WebClient(slack_token)
-                
+
         def send_message(self,message,attachment=None):
 
                 """ Function that sends a message using SLACK API"""
@@ -44,8 +45,36 @@ def slack_mark_notification(mark,app,show_type):
                             }
                         ])
 
-                        # We encode as str in order to avoid SLACK Api Parsing when unfurling the URL
-                        slack_channel.send_message(message="<" + mark.show.url + "?language=fr|" + mark.show.name + ">") 
+                        if show_type == "videogame":
+                                # IGDB links never unfurl (igdb.com returns 403 to bots), so we build
+                                # the preview card ourselves, TMDB-style, with the poster served from
+                                # /static/posters as the thumbnail (reachable on prod, not on a local dev box)
+                                if mark.show.release_date:
+                                        year = " (%d)" % mark.show.release_date.year 
+                                else:
+                                        year = ""
+                                
+                                # Truncate the overview field if it's too long
+                                overview = (mark.show.overview or "").strip()
+                                if len(overview) > 300:
+                                        overview = overview[:299].rsplit(" ", 1)[0] + "…"
+                               
+                                # Let's generate the Slack card
+                                card = {
+                                    "author_name": "IGDB",
+                                    "title": mark.show.name + year,
+                                    "title_link": mark.show.url + "?language=fr",
+                                    "fallback": mark.show.name + year,
+                                    "text": overview
+                                }
+
+                                # Add the poster link if there is one
+                                if mark.show.poster_path:
+                                        card["thumb_url"] = request.url_root.rstrip("/") + app.config["POSTERS_URL"] + mark.show.poster_path
+                                slack_channel.send_message(message="", attachment=json.dumps([card]))
+                        else:
+                                # We encode as str in order to avoid SLACK Api Parsing when unfurling the URL
+                                slack_channel.send_message(message="<" + mark.show.url + "?language=fr|" + mark.show.name + ">")
                         slack_channel.send_message(message="Note de @%s: *%s*" % (mark.user.nickname, str(mark.mark)) ,attachment=attachment)
                         return 0
 
@@ -54,4 +83,3 @@ def slack_mark_notification(mark,app,show_type):
                         return 1
         else:
                 return -1
-
