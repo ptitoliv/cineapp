5669944b42677685e2f0f06322a95453b102a8df
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2026-07-06 00:20:04 +0200

    feat(slack) Refactor Slack notification message
    Since the unfurling method is not working to fetch correctly the poster
    from IMDB when generating the slack notification for a Video game, the
    Slack notification message must be refactored. We are now using the
    Block Kit module and the Section Block component in order to build a new
    message that allows to display correctly the poster on Slack.
    
    In order to keep consistency, this new notification structure is applied
    for Videogames but also TVShows and movies.
    
    Documentation : https://docs.slack.dev/reference/block-kit/blocks/section-block
    
    Fixes #153

--- cineapp/slack.py
+++ cineapp/slack.py
@@ -16,17 +16,18 @@ class SlackChannel(object):
                 # This is here we are going to send the Slack notification
                 self.slack_token = WebClient(slack_token)
 
-        def send_message(self,message,attachment=None):
+        def send_message(self,message,attachment=None,blocks=None):
 
                 """ Function that sends a message using SLACK API"""
                 # Send the message
                 try:
-                    response=self.slack_token.chat_postMessage(
+                    self.slack_token.chat_postMessage(
                       channel=self.channel_name,
                       text=message,
                       attachments=attachment,
+                      blocks=blocks,
                       link_names=1,
-                      unfurl_links=True
+                      unfurl_links=False
                     )
 
                 except Exception as e:
@@ -39,43 +40,58 @@ def slack_mark_notification(mark,app,show_type):
                 try:
                         slack_channel = SlackChannel(app.config["SLACK_TOKEN"],app.config["SLACK_NOTIFICATION_CHANNEL"][show_type])
                         app.logger.info("Notification sur SLACK pour la note de %s sur le show(%s) %s" % (mark.user.nickname,show_type,mark.show.name))
-                        attachment = json.dumps([
-                            {
-                                "text": mark.comment
-                            }
-                        ])
 
-                        if show_type == "videogame":
-                                # IGDB links never unfurl (igdb.com returns 403 to bots), so we build
-                                # the preview card ourselves, TMDB-style, with the poster served from
-                                # /static/posters as the thumbnail (reachable on prod, not on a local dev box)
-                                if mark.show.release_date:
-                                        year = " (%d)" % mark.show.release_date.year 
-                                else:
-                                        year = ""
-                                
-                                # Truncate the overview field if it's too long
-                                overview = (mark.show.overview or "").strip()
-                                if len(overview) > 300:
-                                        overview = overview[:299].rsplit(" ", 1)[0] + "…"
-                               
-                                # Let's generate the Slack card
-                                card = {
-                                    "author_name": "IGDB",
-                                    "title": mark.show.name + year,
-                                    "title_link": mark.show.url + "?language=fr",
-                                    "fallback": mark.show.name + year,
-                                    "text": overview
-                                }
+                        # Same rich Block Kit card for every show type (movie / tvshow / videogame):
+                        # linked title + overview with the poster as a small thumbnail (section
+                        # accessory), the note in a prominent header, then the comment. TMDB links used
+                        # to unfurl natively but we now render the card ourselves everywhere for a
+                        # consistent, reliable preview (Slack no longer renders legacy attachment thumbs).
+                        # Next steps consits in building the local fields that will be used to create the Block
 
-                                # Add the poster link if there is one
-                                if mark.show.poster_path:
-                                        card["thumb_url"] = request.url_root.rstrip("/") + app.config["POSTERS_URL"] + mark.show.poster_path
-                                slack_channel.send_message(message="", attachment=json.dumps([card]))
+                        # Build the comment field                 
+                        comment_text = (mark.comment or "").strip()
+
+                        # Score without a trailing ".0" (15.0 → "15", 15.5 → "15.5").
+                        mark_str = ("%g" % mark.mark) if mark.mark is not None else "?"
+
+                        # Extract the year from the release date                        
+                        if mark.show.release_date:
+                                year = " (%d)" % mark.show.release_date.year
                         else:
-                                # We encode as str in order to avoid SLACK Api Parsing when unfurling the URL
-                                slack_channel.send_message(message="<" + mark.show.url + "?language=fr|" + mark.show.name + ">")
-                        slack_channel.send_message(message="Note de @%s: *%s*" % (mark.user.nickname, str(mark.mark)) ,attachment=attachment)
+                                year = ""
+                                
+                        # Truncate the overview field if it's too long
+                        overview = (mark.show.overview or "").strip()
+                        if len(overview) > 300:
+                                overview = overview[:299].rsplit(" ", 1)[0] + "…"
+                        title = mark.show.name + year
+
+                        # Build the clickable link (Show Title + URL)
+                        section_text = "*<%s|%s>*" % (mark.show.url, title)
+
+                        # Append the overview to the section text
+                        if overview:
+                                section_text += "\n" + overview
+
+                        # Append the mark to the section_text
+                        section_text += "\n\n⭐ *Note de @%s:  %s/20*" % (mark.user.nickname,mark_str)
+
+                        # Now let's build the Section block with the built section_text
+                        section_block = { "type": "section", "text": { "type": "mrkdwn", "text": section_text } }
+
+                        # If we have a poster, build the accessory section
+                        if mark.show.poster_path:
+                                poster_url = request.url_root.rstrip("/") + app.config["POSTERS_URL"] + mark.show.poster_path
+                                section_block = dict(section_block, accessory={ "type": "image", "image_url": poster_url, "alt_text": title })
+                                
+                        # Blocks: the card, then the comment as its own full-width section
+                        # (below the card, not squeezed beside the poster thumbnail).
+                        blocks = [section_block]
+                        if comment_text:
+                                blocks.append({ "type": "section", "text": { "type": "mrkdwn", "text": "> " + comment_text.replace("\n", "\n> ") } })
+
+                        # Send the message
+                        slack_channel.send_message(message=title, blocks=blocks)
                         return 0
 
                 except Exception as e:
